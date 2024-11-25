f089139511a55ae63cb8da515a95b337c822b9b0
krishnasankar <sankarkrishna689@gmail.com>
2024-11-25 22:08:08 +0530

    Changes

---
+++ app/fonts.ts
@@ -0,0 +1,13 @@
+import localFont from "next/font/local";
+
+export const workSans = localFont({
+    src: "./fonts/Work_Sans/WorkSans-VariableFont_wght.ttf",
+    weight: "100 900",
+    display: "swap",
+  })
+  
+export const workSanSItalic = localFont({
+    src: "./fonts/Work_Sans/WorkSans-Italic-VariableFont_wght.ttf",
+    weight: "100 900",
+    display: "swap",
+  })
